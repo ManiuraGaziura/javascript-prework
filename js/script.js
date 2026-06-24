2c62645992170b319921ef8864e41b7363325e93
Maniek Gaznik <maniekgaznik@gmail.com>
2026-06-24 14:38:21 +0200

    script losowe ruchy 0.3

--- js/script.js
+++ js/script.js
@@ -8,6 +8,17 @@ function clearMessages() {
   document.getElementById('messages').innerHTML = '';
 }
 
-var randomNumber = Math.floor(Math.random() * 9 + 11);
+var randomNumber = Math.floor(Math.random() * 3 + 1);
+var computerMove = '';
 
-printMessage('Wylosowana liczba to: ' + randomNumber);
+console.log('Wylosowana liczba to: ' + randomNumber);
+
+if (randomNumber == 1) {
+  computerMove = 'kamień';
+} else if (randomNumber == 2) {
+  computerMove = 'papier';
+} else if (randomNumber == 3) {
+  computerMove = 'nożyce';
+}
+
+printMessage('Mój ruch: ' + computerMove);
